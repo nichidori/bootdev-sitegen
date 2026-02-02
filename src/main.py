c471e2d646525651d8217deeb97a218eaf81f203
Arrafly Dori <arraflydori@gmail.com>
2026-02-02 22:23:28 +0700

    fix ch5 l1

--- src/main.py
+++ src/main.py
@@ -14,22 +14,22 @@ def copy_content(directory, src, dst):
         return
 
     if os.path.exists(dst_path):
-        print(f"Removing existing directory {dst_path}")
+        print(f"Removing existing directory {dst}")
         shutil.rmtree(dst_path)
 
-    print(f"Creating directory {dst_path}")
+    print(f"Creating directory {dst}")
     os.mkdir(dst_path)
 
     entries = os.listdir(src_path)
     for entry in entries:
+        entry_src = os.path.join(src, entry)
+        entry_dst = os.path.join(dst, entry)
         full_src = os.path.join(src_path, entry)
         full_dst = os.path.join(dst_path, entry)
         if os.path.isdir(full_src):
-            entry_src = os.path.join(src, entry)
-            entry_dst = os.path.join(dst, entry)
             copy_content(directory, entry_src, entry_dst)
         else:
-            print(f"Copying file from {full_src} to {full_dst}")
+            print(f"Copying file from {entry_src} to {entry_dst}")
             shutil.copy(full_src, full_dst)
 
     print(f"Finished copying {src} to {dst}")
