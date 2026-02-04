1e98cda12375ddc0f301018207bfe32b0e226c79
Arrafly Dori <arraflydori@gmail.com>
2026-02-04 22:56:43 +0700

    ch5 l4

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,5 @@
+import os
+
 from extract_title import extract_title
 from markdown_to_html_node import markdown_to_html_node
 
@@ -14,11 +16,24 @@ def generate_page(from_path, template_path, dest_path):
         markdown_content = file.read()
     with open(template_path, "r") as file:
         template_content = file.read()
-    
+
     content = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
-    
-    final_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", content)
-    
+
+    final_html = template_content.replace("{{ Title }}", title).replace(
+        "{{ Content }}", content
+    )
+
     with open(dest_path, "w") as file:
         file.write(final_html)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry.replace(".md", ".html"))
+        if os.path.isfile(from_path) and entry.endswith(".md"):
+            generate_page(from_path, template_path, dest_path)
+        elif os.path.isdir(from_path):
+            os.makedirs(dest_path, exist_ok=True)
+            generate_pages_recursive(from_path, template_path, dest_path)
